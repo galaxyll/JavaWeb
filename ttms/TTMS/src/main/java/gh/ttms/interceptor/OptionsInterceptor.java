37fbae806043a176c9d5084ceb6cb3fce3f975dd
galaxy <galaxy@example.com>
2019-05-25 22:55:47 +0800

    重配了mvc:cros

--- ttms/TTMS/src/main/java/gh/ttms/interceptor/OptionsInterceptor.java
+++ ttms/TTMS/src/main/java/gh/ttms/interceptor/OptionsInterceptor.java
@@ -1,46 +1,46 @@
-package gh.ttms.interceptor;
-
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.List;
-
-public class OptionsInterceptor implements HandlerInterceptor {
-    private List<String> excludedUrls;
-
-    @Override
-    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
-        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
-        httpServletResponse.setHeader("Access-Control-Allow-Methods", "*");
-        httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
-        httpServletResponse.setHeader("Access-Control-Allow-Headers",
-                "Origin, X-Requested-With, Content-Type, Accept");
-        String method = httpServletRequest.getMethod();
-        if (method.equals("OPTIONS")){
-            httpServletResponse.setStatus(200);
-            return false;
-        }
-        System.out.println(method);
-        return true;
-    }
-
-    @Override
-    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
-
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
-
-    }
-
-    public List<String> getExcludedUrls() {
-        return excludedUrls;
-    }
-
-    public void setExcludedUrls(List<String> excludedUrls) {
-        this.excludedUrls = excludedUrls;
-    }
-}
+//package gh.ttms.interceptor;
+//
+//import org.springframework.web.servlet.HandlerInterceptor;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.util.List;
+//
+//public class OptionsInterceptor implements HandlerInterceptor {
+//    private List<String> excludedUrls;
+//
+//    @Override
+//    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+//        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+//        httpServletResponse.setHeader("Access-Control-Allow-Methods", "*");
+//        httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
+//        httpServletResponse.setHeader("Access-Control-Allow-Headers",
+//                "Origin, X-Requested-With, Content-Type, Accept");
+//        String method = httpServletRequest.getMethod();
+//        if (method.equals("OPTIONS")){
+//            httpServletResponse.setStatus(200);
+//            return false;
+//        }
+//        System.out.println(method);
+//        return true;
+//    }
+//
+//    @Override
+//    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+//
+//    }
+//
+//    @Override
+//    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+//
+//    }
+//
+//    public List<String> getExcludedUrls() {
+//        return excludedUrls;
+//    }
+//
+//    public void setExcludedUrls(List<String> excludedUrls) {
+//        this.excludedUrls = excludedUrls;
+//    }
+//}
