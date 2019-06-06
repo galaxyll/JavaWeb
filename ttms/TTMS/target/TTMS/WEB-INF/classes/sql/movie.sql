43490ac5a0fa5bd14cff4a8bebffa3673a76a1af
galaxy <galaxy@example.com>
2019-06-06 15:32:45 +0800

    改大了movie表

--- ttms/TTMS/target/TTMS/WEB-INF/classes/sql/movie.sql
+++ ttms/TTMS/target/TTMS/WEB-INF/classes/sql/movie.sql
@@ -3,13 +3,12 @@ drop table movie;
 create table movie(
   id int primary key AUTO_INCREMENT,
   moviename varchar(50) not null unique,
-  photo  varchar(50),
-  type varchar(50) default "暂无分类" not null,
+  photo  varchar(100),
+  type varchar(100) default "暂无分类" not null,
   duration int default 0 not null,
-  introduction varchar(200) default "暂无简介。" not null,
+  introduction varchar(400) default "暂无简介。" not null,
   release_date date default "2020-12-31" not null,
   score float(1) default 0,
-  author char(20) default "暂无作者" not null
+  author varchar(100) default "暂无作者" not null,
+  production varchar(400)
 );
-
-alter table movie add column  production varchar(200);
